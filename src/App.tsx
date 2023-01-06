818d53a3995cfdfe1fabce92c998f2e088fcda61
dolgovdv <dolgov.denis@gmail.com>
2023-01-06 18:54:11 +0300

    feat(app): 7.1
    добавил:
    - переключатель темы через контекст

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React, {Suspense} from 'react';
+import React, {Suspense, useContext, useState} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
-import './index.scss'
+import './styles/index.scss'
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {Theme, ThemeContext, ThemeContextProps} from "./theme/ThemeContext";
+
 
 const MyComponent = () => {
+    const {theme, setTheme} = useContext<ThemeContextProps>(ThemeContext)
+
+
+    const toggleTheme = () => {
+        setTheme((prevState: Theme) => prevState === Theme.LIGHT ? Theme.DARK : Theme.LIGHT)
+    }
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button type={"button"} onClick={toggleTheme}>Theme</button>
             <Link to={'/'}>Main</Link>
             <Link to={'/about'}>About</Link>
             <Suspense fallback={<div>Загрузка...</div>}>
---
+++ src/theme/ThemeContext.ts
@@ -0,0 +1,15 @@
+import {createContext} from "react";
+
+export enum Theme {
+    LIGHT = 'light',
+    DARK = 'dark'
+}
+
+export interface ThemeContextProps {
+    theme?: Theme,
+    setTheme?: (theme: (prevState: Theme) => Theme.DARK | Theme.LIGHT) => void
+
+}
+export const ThemeContext = createContext<ThemeContextProps>({})
+
+export const LOCAL_STORAGE_THEME_KEY = 'theme'

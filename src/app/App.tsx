e2a3e2bf0ac9550769392ff18b233d6f1c21b478
dolgovdv <dolgov.denis@gmail.com>
2023-01-21 19:25:06 +0300

    refactor(app): 10-3
    Настройка экспортов для pages, рефакторинг каталогов.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
 import './styles/index.scss'
-import {AboutPageAsync} from "pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "pages/MainPage/MainPage.async";
 import {useTheme} from "app/providers/ThemeProvider";
-import {classNames} from "helpers/classNames/classNames";
+import {classNames} from "shared/lib/classNames/classNames";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 
 const MyComponent = () => {
@@ -17,8 +17,8 @@ const MyComponent = () => {
             <Link to={'/about'}>About</Link>
             <Suspense fallback={<div>Загрузка...</div>}>
                 <Routes>
-                    <Route path='/about' element={<AboutPageAsync/>}/>
-                    <Route path='/' element={<MainPageAsync/>}/>
+                    <Route path='/about' element={<AboutPage/>}/>
+                    <Route path='/' element={<MainPage/>}/>
                 </Routes>
             </Suspense>
         </div>

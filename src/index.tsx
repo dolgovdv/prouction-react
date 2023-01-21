2d85b8e085c99307772ecdfe43be5c61e0c4db75
dolgovdv <dolgov.denis@gmail.com>
2023-01-21 19:01:02 +0300

    feat(app): 10-1
    Привел к структуре FSD
    добавил:
    
    изменения:
    - в структуре файлов

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import {render} from "react-dom";
-import App from "./App";
+import App from "./app/App";
+import {ThemeProvider} from "app/providers/ThemeProvider";
 import {BrowserRouter} from "react-router-dom";
-import {ThemeProvider} from "./theme/ThemeProvider";
 
 render(
     <BrowserRouter>

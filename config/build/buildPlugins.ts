a17bd424359d3c277f296788bf3ebe42c67132e6
dolgovdv <dolgov.denis@gmail.com>
2023-02-25 17:06:15 +0300

    fix(github-actions): удалил bandle-analizer для prod сборки

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -17,9 +17,6 @@ export function buildPlugins ({ paths, isDev }: BuildOptions): webpack.WebpackPl
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev)
-    }),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false
     })
   ]
   if (isDev) {

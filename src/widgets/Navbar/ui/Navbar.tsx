abad033df39977761ec8216bd04ea9fde1a02fb8
dolgovdv <dolgov.denis@gmail.com>
2023-02-27 19:15:19 +0300

    28_3 feat(Sidebar): перенес ссылки на сайдбар, настроил иконки при сворачивании.

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,21 +1,13 @@
-import { classNames } from 'shared/lib/classNames/classNames'
+import {classNames} from 'shared/lib/classNames/classNames'
 import cls from './Navbar.module.scss'
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
-import { type FC } from 'react'
-import { useTranslation } from 'react-i18next'
+// import {AppLink, AppLinkTheme} from 'shared/ui/AppLink/AppLink'
+import {type FC} from 'react'
+// import {useTranslation} from 'react-i18next'
 
 interface NavbarProps {
-  className?: string
+    className?: string
 }
 
-export const Navbar: FC<NavbarProps> = ({ className = '' }) => {
-  const { t } = useTranslation()
-  return (
-        <div className={classNames(cls.navbar, {}, [className])}>
-            <div className={cls.links}>
-                <AppLink to={'/'} className={cls.main}>{t('Main')}</AppLink>
-                <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>{t('About')}</AppLink>
-            </div>
-        </div>
-  )
+export const Navbar: FC<NavbarProps> = ({className = ''}) => {
+    return <div className={classNames(cls.navbar, {}, [className])}></div>
 }
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -4,12 +4,17 @@ import {type FC, useState} from 'react'
 import {ThemeSwitcher} from 'widgets/ThemeSwitcher'
 import {LangSwitcher} from 'widgets/LangSwitcher'
 import {Button, ButtonTheme} from 'shared/ui/Button/Button'
+import {AppLink, AppLinkTheme} from 'shared/ui/AppLink/AppLink'
+import {useTranslation} from 'react-i18next'
+import MainIcon from 'shared/assets/icons/main.svg'
+import AboutIcon from 'shared/assets/icons/about.svg'
 
 interface SidebarProps {
     className?: string
 }
 
 export const Sidebar: FC<SidebarProps> = ({className = ''}) => {
+    const {t} = useTranslation()
     const [collapsed, setCollapsed] = useState<boolean>(false)
     const onToggle = (): void => {
         setCollapsed((prevState) => !prevState)
@@ -21,6 +26,32 @@ export const Sidebar: FC<SidebarProps> = ({className = ''}) => {
                 className,
             ])}
         >
+            <div className={cls.items}>
+                <AppLink to={'/'} className={cls.item}>
+                    <MainIcon className={cls.icon} />
+                    <span className={cls.link}>{t('Main')}</span>
+                </AppLink>
+
+                <AppLink
+                    to={'/about'}
+                    theme={AppLinkTheme.SECONDARY}
+                    className={cls.item}
+                >
+                    <AboutIcon className={cls.icon} />
+                    <span className={cls.link}>{t('About')}</span>
+                </AppLink>
+
+                {/* <div className={cls.item}> */}
+                {/*    <AboutIcon className={cls.icon} /> */}
+                {/*    <AppLink */}
+                {/*        theme={AppLinkTheme.SECONDARY} */}
+                {/*        to={'/about'} */}
+                {/*        className={cls.link} */}
+                {/*    > */}
+                {/*        {t('About')} */}
+                {/*    </AppLink> */}
+                {/* </div> */}
+            </div>
             <Button
                 data-testid='sidebar-toggle'
                 type='button'

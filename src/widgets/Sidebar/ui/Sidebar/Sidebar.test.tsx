e102cc779d419e426d96b07fe6d1e54ca45d4019
dolgovdv <dolgov.denis@gmail.com>
2023-02-28 14:28:00 +0300

    28_5 feat(rtl): изменил обертку для тестов для рендеринга компонентов

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,18 +1,18 @@
-import { fireEvent, screen } from '@testing-library/react'
-import { Sidebar } from 'widgets/Sidebar'
-import { renderWithTranslation } from 'shared/tests/renderWithTranslation/renderWithTranslation'
+import {fireEvent, screen} from '@testing-library/react'
+import {Sidebar} from 'widgets/Sidebar'
+import {ComponentRender} from 'shared/config/tests/ComponentRender/ComponentRender'
 
 describe('Sidebar', function () {
-  test('first test', () => {
-    renderWithTranslation(<Sidebar/>)
-    expect(screen.getByTestId('sidebar')).toBeInTheDocument()
-  })
+    test('first test', () => {
+        ComponentRender(<Sidebar />)
+        expect(screen.getByTestId('sidebar')).toBeInTheDocument()
+    })
 
-  test('first test', () => {
-    renderWithTranslation(<Sidebar/>)
-    const toggleBtn = screen.getByTestId('sidebar-toggle')
-    expect(screen.getByTestId('sidebar')).toBeInTheDocument()
-    fireEvent.click(toggleBtn)
-    expect(screen.getByTestId('sidebar')).toHaveClass('collapsed')
-  })
+    test('first test', () => {
+        ComponentRender(<Sidebar />)
+        const toggleBtn = screen.getByTestId('sidebar-toggle')
+        expect(screen.getByTestId('sidebar')).toBeInTheDocument()
+        fireEvent.click(toggleBtn)
+        expect(screen.getByTestId('sidebar')).toHaveClass('collapsed')
+    })
 })

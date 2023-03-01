c99e7497fed405ff5073f1a1ffe9d2a041c97a9c
dolgovdv <dolgov.denis@gmail.com>
2023-03-01 19:30:56 +0300

    29_42 feat(loki): вернул в ThemeSwitcher для темной темы

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.stories.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.stories.tsx
@@ -1,7 +1,7 @@
 import {type ComponentMeta, type ComponentStory} from '@storybook/react'
 import 'app/styles/variables/global.scss'
-// import {ThemeDecorator} from 'shared/config/storybook/ThemeDecorator/ThemeDecorator'
-// import {Theme} from 'app/providers/ThemeProvider'
+import {ThemeDecorator} from 'shared/config/storybook/ThemeDecorator/ThemeDecorator'
+import {Theme} from 'app/providers/ThemeProvider'
 import {ThemeSwitcher} from './ThemeSwitcher'
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
@@ -18,11 +18,11 @@ const Template: ComponentStory<typeof ThemeSwitcher> = (args) => <ThemeSwitcher 
 export const Light = Template.bind({})
 Light.args = {}
 
-// export const Dark = Template.bind({})
-// Dark.args = {}
+export const Dark = Template.bind({})
+Dark.args = {}
 // TODO: тесты падают с ошибкой
 // No reference image found
 // You can update the reference files with:
 // loki update --storiesFilter="^widgets\\/ThemeSwitcher Dark\$" --reactUri="file:./storybook-static"
 // Error: Process completed with exit code 1.
-// Dark.decorators = [ThemeDecorator(Theme.DARK)]
+Dark.decorators = [ThemeDecorator(Theme.DARK)]

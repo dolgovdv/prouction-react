fe31dc66151d5e26d80c0ce9e7e5a984d01d39bd
dolgovdv <dolgov.denis@gmail.com>
2023-11-23 20:57:42 +0300

    44 - добавление новой цветовой темы

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -9,7 +9,20 @@ export function useTheme(): UseThemeResult {
     const {theme, setTheme} = useContext<ThemeContextProps>(ThemeContext)
 
     const toggleTheme = (): void => {
-        const newTheme = theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT
+        let newTheme: Theme
+        switch (theme) {
+            case Theme.DARK:
+                newTheme = Theme.LIGHT
+                break
+            case Theme.LIGHT:
+                newTheme = Theme.ORANGE
+                break
+            case Theme.ORANGE:
+                newTheme = Theme.DARK
+                break
+            default:
+                newTheme = Theme.LIGHT
+        }
         setTheme?.(newTheme)
         /**
          * передаем тему сразу в body.
